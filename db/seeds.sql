9bc545df0ca3cab5893bc7e95a05d362fc57a313
KSentak <k.sentak@outlook.com>
2019-11-23 09:41:06 -0500

    Created a mysql database using code within schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quesadilla Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Shake Shack Burger');
