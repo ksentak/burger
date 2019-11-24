9e736248d05e954b6b7df0e495a1a6e5574fdd38
KSentak <k.sentak@outlook.com>
2019-11-24 17:43:38 -0500

    Added .env file to protect data

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.DATABASE
 });
 
 // Make connection.
