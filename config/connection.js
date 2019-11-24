581f160d1ffadfd212224f030c8184559523b386
KSentak <k.sentak@outlook.com>
2019-11-24 17:57:13 -0500

    Added new connection for heroku deployment of mysql

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require("dotenv").config();
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+
+else { 
+  connection = mysql.createConnection({
   host: process.env.HOST,
   port: process.env.PORT,
   user: process.env.USER,
   password: process.env.MYSQL_PWD,
   database: process.env.DATABASE
-});
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
